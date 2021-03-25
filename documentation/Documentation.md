8570d2b367299f7a4429b4754bc709d731704afc
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-24 23:09:40 -0700

    update api tests to include additional tests and improve all test coverage to 80%

---
+++ documentation/Documentation.md
@@ -0,0 +1,18 @@
+# Documentation
+
+## NPM Packages
+
+1. eslint@7.22.0
+2. nyc@15.1.0
+3. pre-push@0.1.1
+4. winston@3.3.3
+
+## Test Coverage
+
+### 2021-03-25 (initial)
+
+![Test Coverage Initial](./screenshots/Test_Coverage_2021-03-24.png)
+
+### 2021-03-25 (after improvement)
+
+![Test Coverage After Improvement](./screenshots/Test_Coverage_80_percent_2021-03-24.png)
--- tests/api.test.js
+++ tests/api.test.js
@@ -29,4 +29,78 @@ describe('API tests', () => {
                 .expect(200, done);
         });
     });
+
+    describe('POST /rides', () => {
+        it('should return insert a ride and return the inserted ride', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 2, start_long: 1, end_lat: 3, end_long: 4, rider_name: "John", driver_name: "Jim", driver_vehicle: "Bicycle"})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should return insert a ride and return the inserted ride', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 91, start_long: 181, end_lat: 3, end_long: 4, rider_name: "John", driver_name: "Jim", driver_vehicle: "Bicycle"})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should return insert a ride and return the inserted ride', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 1, start_long: 1, end_lat: 91, end_long: 181, rider_name: "John", driver_name: "Jim", driver_vehicle: "Bicycle"})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should return insert a ride and return the inserted ride with invalid rider datatype', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 1, start_long: 1, end_lat: 1, end_long: 1, rider_name: "", driver_name: "Jim", driver_vehicle: "Bicycle"})
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Rider name must be a non empty string'
+                }, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should return insert a ride and return the inserted ride with invalid driver datatype', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 1, start_long: 1, end_lat: 1, end_long: 1, rider_name: "John", driver_name: "", driver_vehicle: "Bicycle"})
+                .expect('Content-Type', /json/)
+                .expect(200, {
+                    error_code: 'VALIDATION_ERROR',
+                    message: 'Rider name must be a non empty string'
+                }, done);
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return all rides', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /rides/:id', () => {
+        it('should return a ride by id', (done) => {
+            request(app)
+                .get('/rides/1')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
 });
